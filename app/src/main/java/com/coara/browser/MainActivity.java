a9bc016abb68339bfa21cfe9cbbe04d808172aa3
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-08-25 05:16:46 +0900

    Update MainActivity.java

--- app/src/main/java/com/coara/browser/MainActivity.java
+++ app/src/main/java/com/coara/browser/MainActivity.java
@@ -87,6 +87,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Collections;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
