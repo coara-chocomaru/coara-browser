a42c880fdf122ca85dd9290d0a4767338bf5e2ff
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-08-24 19:20:56 +0900

    Update Android.mk

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -1,25 +1,28 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := openssl_crypto
-LOCAL_SRC_FILES := libs/libcrypto.so
+LOCAL_MODULE := crypto
+LOCAL_SRC_FILES := libs/$(TARGET_ARCH_ABI)/libcrypto.so
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include/openssl
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := png
-LOCAL_SRC_FILES := libs/libpng.so
+LOCAL_SRC_FILES := libs/$(TARGET_ARCH_ABI)/libpng.so
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include/png
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := zlib
-LOCAL_SRC_FILES := libs/libz.so
+LOCAL_SRC_FILES := libs/$(TARGET_ARCH_ABI)/libz.so
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include/zlib
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := browseropt
 LOCAL_SRC_FILES := browseropt.cpp
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/include
+LOCAL_SHARED_LIBRARIES := crypto png zlib
 LOCAL_LDLIBS := -llog -landroid
-LOCAL_SHARED_LIBRARIES := openssl_crypto png zlib
-LOCAL_CFLAGS := -O3 -march=armv8-a+simd -fPIC -Wall -Wextra -Wno-unused-parameter -fno-exceptions -fno-rtti
+LOCAL_CFLAGS := -std=c++11 -fexceptions -frtti
+LOCAL_CPPFLAGS := -std=c++11 -fexceptions -frtti
 include $(BUILD_SHARED_LIBRARY)
