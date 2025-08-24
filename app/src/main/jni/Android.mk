1b1a1766e9f6b8c96ad3d78d505dca49ca601416
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-08-24 21:31:15 +0900

    Update Android.mk

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -1,32 +1,15 @@
 LOCAL_PATH := $(call my-dir)
 
-
 include $(CLEAR_VARS)
-LOCAL_MODULE := crypto
-LOCAL_SRC_FILES := libs/$(TARGET_ARCH_ABI)/libcrypto.so
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include/openssl
-include $(PREBUILT_SHARED_LIBRARY)
 
+LOCAL_MODULE    := browseropt
+LOCAL_SRC_FILES := BrowserOptService.cpp
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := png
-LOCAL_SRC_FILES := libs/$(TARGET_ARCH_ABI)/libpng.so
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
-include $(PREBUILT_SHARED_LIBRARY)
+LOCAL_LDLIBS := -llog -landroid -lz
 
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../../../deps_build/install/$(TARGET_ARCH_ABI)/include
+LOCAL_LDFLAGS    += -L$(LOCAL_PATH)/../../../../deps_build/install/$(TARGET_ARCH_ABI)/lib
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := z
-LOCAL_SRC_FILES := libs/$(TARGET_ARCH_ABI)/libz.so
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
-include $(PREBUILT_SHARED_LIBRARY)
+LOCAL_LDLIBS    += -lssl -lcrypto -lpng16
 
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := browseropt
-LOCAL_SRC_FILES := browseropt.cpp
-LOCAL_SHARED_LIBRARIES := crypto png z
-LOCAL_LDLIBS := -llog -landroid
-LOCAL_CFLAGS := -std=c++11 -fexceptions -frtti
-LOCAL_CPPFLAGS := -std=c++11 -fexceptions -frtti
 include $(BUILD_SHARED_LIBRARY)
