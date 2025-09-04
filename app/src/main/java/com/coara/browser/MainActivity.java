eef132d489c203c391bb08db29c5ee104e6330c0
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-09-04 14:49:55 +0900

    Update MainActivity.java

--- app/src/main/java/com/coara/browser/MainActivity.java
+++ app/src/main/java/com/coara/browser/MainActivity.java
@@ -1234,9 +1234,9 @@ public class MainActivity extends AppCompatActivity {
                     HttpURLConnection conn = (HttpURLConnection) u.openConnection();
                     conn.setRequestMethod("GET");
                     String cookies = CookieManager.getInstance().getCookie(url);
-                    conn.addRequestHeader("cookie", cookies);
+                    conn.setRequestProperty("cookie", cookies);
                     if (userAgent != null) {
-                        conn.addRequestHeader("User-Agent", userAgent);
+                        conn.setRequestProperty("User-Agent", userAgent);
                     }
                     conn.connect();
                     if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
