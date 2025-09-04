711a4831266ccf300464867eebe0267fd59ac9db
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-09-04 14:21:05 +0900

    Update SecretActivity.java

--- app/src/main/java/com/coara/browser/SecretActivity.java
+++ app/src/main/java/com/coara/browser/SecretActivity.java
@@ -262,6 +262,9 @@ public class SecretActivity extends AppCompatActivity {
         }
     }
 };
+        CookieManager cookieManager = CookieManager.getInstance();
+        cookieManager.removeAllCookies(null);
+        cookieManager.flush();
         loadBookmarks();
         loadHistory();
         if (!historyItems.isEmpty()) {
@@ -2320,33 +2323,9 @@ private void showHistoryDialog() {
     }
     private void loadHistory() {
     historyItems.clear();
-    String jsonStr = pref.getString(KEY_HISTORY, "[]");
-    try {
-        JSONArray array = new JSONArray(jsonStr);
-        for (int i = 0; i < array.length(); i++) {
-            JSONObject obj = array.getJSONObject(i);
-            String title = obj.getString("title");
-            String url = obj.getString("url");
-            historyItems.add(new HistoryItem(title, normalizeUrl(url))); 
-        }
-    } catch (JSONException e) {
-        e.printStackTrace();
-    }
 }
 
 private void saveHistory() {
-    JSONArray array = new JSONArray();
-    for (HistoryItem item : historyItems) {
-        JSONObject obj = new JSONObject();
-        try {
-            obj.put("title", item.getTitle());
-            obj.put("url", item.getUrl());
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        array.put(obj);
-    }
-    pref.edit().putString(KEY_HISTORY, array.toString()).apply();
 }
 
 private String normalizeUrl(String url) {  
@@ -2364,13 +2343,10 @@ private void addHistory(String url, String title) {
     String normalizedUrl = normalizeUrl(url);
     if (!historyItems.isEmpty() && normalizeUrl(historyItems.get(historyItems.size() - 1).getUrl()).equals(normalizedUrl))
         return;
-    backgroundExecutor.execute(() -> { 
-        historyItems.add(new HistoryItem(title, url));
-        if (historyItems.size() > MAX_HISTORY_SIZE) {
-            historyItems.remove(0);
-        }
-        saveHistory();
-    });
+    historyItems.add(new HistoryItem(title, url));
+    if (historyItems.size() > MAX_HISTORY_SIZE) {
+        historyItems.remove(0);
+    }
 }
     public void exitFullScreen() {
         if (customView != null) {
