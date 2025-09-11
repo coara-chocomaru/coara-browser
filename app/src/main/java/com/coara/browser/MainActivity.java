9ec1a4651e075830af46db18773037a9e8f68dee
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-09-12 06:13:28 +0900

    Update MainActivity.java

--- app/src/main/java/com/coara/browser/MainActivity.java
+++ app/src/main/java/com/coara/browser/MainActivity.java
@@ -130,7 +130,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String APPEND_STR = " CoaraBrowser";
     private static final String START_PAGE = "file:///android_asset/index.html";
     private static final int FILE_SELECT_CODE = 1001;
-    private static final int MAX_TABS = 30;
+    private static final int MAX_TABS = 10;
     private static final int MAX_HISTORY_SIZE = 100;
     private static final String SENTINEL_FILENAME = "cache_sentinel.txt";
     public static final String EXTRA_CLEAR_HISTORY = "com.coara.browser.EXTRA_CLEAR_HISTORY";
