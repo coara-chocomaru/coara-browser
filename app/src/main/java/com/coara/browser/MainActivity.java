10a101544a93ad7f868d1a9d7794f346f0f3d401
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-09-20 14:37:40 +0900

    Add files via upload

--- app/src/main/java/com/coara/browser/MainActivity.java
+++ app/src/main/java/com/coara/browser/MainActivity.java
@@ -114,6 +114,7 @@ public class MainActivity extends AppCompatActivity {
 
     private static final Pattern CACHE_MODE_PATTERN = Pattern.compile("(^|[/.])(?:(chatx2|chatx|chat|auth|nicovideo|login|disk|cgi|session|cloud))($|[/.])", Pattern.CASE_INSENSITIVE);
     private static final String PREF_NAME = "AdvancedBrowserPrefs";
+    private static final String KEY_BACKGROUND_IMAGE_URI = "background_image_uri";
     private static final String KEY_CURRENT_TAB_ID = "current_tab_id";
     private static final String KEY_DARK_MODE = "dark_mode";
     private static final String KEY_BASIC_AUTH = "basic_auth";
