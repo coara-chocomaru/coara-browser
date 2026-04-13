b48f0ff23cfead09a771ac81572c4cfaab9c6a47
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-04-14 07:28:28 +0900

    Add files via upload

--- app/src/main/java/com/coara/browser/model/HistoryItem.java
+++ app/src/main/java/com/coara/browser/model/HistoryItem.java
@@ -3,10 +3,16 @@ package com.coara.browser.model;
 public class HistoryItem {
     private final String title;
     private final String url;
+    private final long timestamp;
 
     public HistoryItem(String title, String url) {
+        this(title, url, System.currentTimeMillis());
+    }
+
+    public HistoryItem(String title, String url, long timestamp) {
         this.title = title;
         this.url = url;
+        this.timestamp = timestamp;
     }
 
     public String getTitle() {
@@ -15,5 +21,9 @@ public class HistoryItem {
 
     public String getUrl() {
         return url;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
     }
 }
